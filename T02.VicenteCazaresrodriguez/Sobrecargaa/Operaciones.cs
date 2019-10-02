33900c441e24e84e9073c76306d843cd39cc09da
Vicente Cazares <56053593+VicenteCazaresRodriguezItt@users.noreply.github.com>
2019-10-01 17:31:53 -0700

    Add files via upload

---
+++ T02.VicenteCazaresrodriguez/Sobrecargaa/Operaciones.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sobrecargaa
+{
+    public class Operaciones
+    {
+
+        public void Sumas()
+        {
+            //iniciamos la sobre carga
+            //los numeros dentro del parentesis seran sustituidos en la operaciones, la sobre carga 
+            //se hara cargo de poner los numeros correctos a sumar
+            double res1 = Suma(4, 6, 2);
+            Console.WriteLine("La respuesta es: " + res1);
+            Console.ReadKey();
+            //solo se usaran 2 datos
+            double res2 = Suma(4, 6);
+            Console.WriteLine("La respuesta es: " + res2);
+            //read key para que cada vez que aplastemos enter se haga una por una y no salga todo de golpe
+            Console.ReadKey();
+
+            double res3 = Suma(4);
+            Console.WriteLine("La respuesta es: " + res3);
+            Console.ReadKey();
+
+        }
+
+        public  double Suma(int a, int b, int c)
+        {
+            //return para que retorne valor
+            return a + b + c;
+        }
+
+        public  double Suma(int a, int b)
+        {
+            return a + b;
+        }
+
+        public  double Suma(int a)
+        {
+            return a ;
+        }
+    }
+}
---
+++ T02.VicenteCazaresrodriguez/Sobrecargaa/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sobrecargaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciamos el metodo 
+            Operaciones oper = new Operaciones();
+            //ponemos mensajes para que los vea el usuario y haga lo dicho
+            Console.WriteLine("Los numeros que se usaran son: 4, 6 y 2.");
+            Console.WriteLine("presiona enter para comenzar");
+            Console.ReadKey();
+            //llamamos a el subprograma despues de presionar una tecla
+            oper.Sumas();
+        }
+    }
+}
